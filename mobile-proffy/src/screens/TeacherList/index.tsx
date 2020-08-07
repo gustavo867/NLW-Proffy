b15816453dbd3e080c071fee3145726932c06e1d
gustavo867 <tartatavo3@gmail.com>
2020-08-07 17:50:19 -0300

    :tada: adding picker in mobile

--- mobile-proffy/src/screens/TeacherList/index.tsx
+++ mobile-proffy/src/screens/TeacherList/index.tsx
@@ -1,14 +1,151 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { View, Text, ScrollView, TextInput, Modal } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Picker from '../../components/Picker';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
 const TeacherList: React.FC = () => {
+  const weekdays = [
+    { label: 'Domingo', value: 0 },
+    { label: 'Segunda', value: 1 },
+    { label: 'Terça', value: 2 },
+    { label: 'Quarta', value: 3 },
+    { label: 'Quinta', value: 4 },
+    { label: 'Sexta', value: 5 },
+    { label: 'Sabádo', value: 6 },
+  ];
+
+  const subjects = [
+    { value: 'Artes', label: 'Artes' },
+    { value: 'Biologia', label: 'Biologia' },
+    { value: 'Ciências', label: 'Ciências' },
+    { value: 'Educação Física', label: 'Educação Física' },
+    { value: 'Física', label: 'Física' },
+    { value: 'Geografia', label: 'Geografia' },
+    { value: 'História', label: 'História' },
+    { value: 'Matemática', label: 'Matemática' },
+    { value: 'Português', label: 'Português' },
+    { value: 'Química', label: 'Química' },
+  ];
+
+  const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+  const [subject, setSubject] = useState();
+  const [week_day, setWeekDay] = useState();
+  const [time, setTime] = useState('');
+  
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id
+        })
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
+  function handleToggleFiltersVisible() {
+    setIsFiltersVisible(!isFiltersVisible)
+  }
+
+  async function handleFiltersSubmit() {
+    loadFavorites();
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+    setIsFiltersVisible(false)
+    setTeachers(response.data)
+  }
+  
   return (
     <View style={styles.container}> 
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader 
+      title="Proffys disponíveis" 
+      headerRight={(
+        <BorderlessButton onPress={handleToggleFiltersVisible}>
+          <Feather name="filter" size={20} color="#FFF"/>
+        </BorderlessButton>
+      )}>
+        { isFiltersVisible && ( 
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <Picker
+              title="Selecione a Matéria"
+              pickerValues={subjects}
+              placeholder="Qual Matéria"
+              inputStyle={styles.pickerInput}
+              onChange={setSubject}
+            />
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <Picker
+                  title="Selecione o dia da semana"
+                  pickerValues={weekdays}
+                  placeholder="Qual dia?"
+                  inputStyle={styles.pickerInput}
+                  onChange={setWeekDay}
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
+                  placeholder="Qual horário?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+            </View>
+
+            <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
+
+      <ScrollView 
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
